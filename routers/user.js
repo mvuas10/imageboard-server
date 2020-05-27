bfc20d1016b6ae3fd90bf052473462cbef7b6002
mvuas10 <mvuas10@gmail.com>
2020-05-27 14:58:52 +0200

    Add user POST

--- routers/user.js
+++ routers/user.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const { Router } = express;
+const bcrypt = require("bcrypt");
 
 const User = require("../models").user;
 
@@ -9,6 +10,25 @@ users.get("/", async (req, res, next) => {
   try {
     const allUsers = await User.findAll();
     res.json(allUsers);
+  } catch (error) {
+    next(error);
+  }
+});
+
+users.post("/", async (req, res, next) => {
+  try {
+    const { fullName, email, password } = req.body;
+    if (!fullName || !email || !password) {
+      res.status(400).send("Missing information");
+    } else {
+      const hashedPassword = bcrypt.hashSync(password, 10);
+      const newUser = await User.create({
+        fullName,
+        email,
+        password: hashedPassword,
+      });
+      res.json(newUser);
+    }
   } catch (error) {
     next(error);
   }
